cfe98c75291900026abb5955145a64a0afc545fe
Sameer <sameerthq2912@gmail.com>
2023-05-11 02:23:22 +0530

    ui changes for login, signup

--- app/src/main/java/com/example/attendme/screens/LoginScreen.kt
+++ app/src/main/java/com/example/attendme/screens/LoginScreen.kt
@@ -1,86 +1,141 @@
 package com.example.attendme.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.attendme.R
 import com.example.attendme.navigation.Screen
+import com.example.attendme.ui.theme.whiteBackground
 import com.example.attendme.viewModels.LoginScreenViewModel
 
 
 @Composable
-fun LoginScreen(navHostController: NavHostController){
-    val viewModel : LoginScreenViewModel = viewModel()
+fun LoginScreen(navController: NavHostController){
+    val viewModel: LoginScreenViewModel = viewModel()
     var passwordVisible by remember { mutableStateOf(false) }
     val context = LocalContext.current
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
-            OutlinedTextField(
-                value = viewModel.email.value,
-                label = { Text(text = "Email") },
-                onValueChange = {
-                    viewModel.email.value = it
-                },
-                modifier = Modifier.padding(bottom = 10.dp)
+    val focusRequester = remember { FocusRequester() }
+    val image = painterResource(id = R.drawable.login_image)
+    val focusManager = LocalFocusManager.current
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White), contentAlignment = Alignment.TopCenter
+        ) {
+
+            Image(
+                image,
+                "",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.size(300.dp)
             )
-
-            OutlinedTextField(value = viewModel.password.value,
-                label = { Text(text = "Password") },
-                onValueChange = {
-                    viewModel.password.value = it
-                },
-                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-                trailingIcon = {
-                    val image = if (passwordVisible)
-                        R.drawable.baseline_visibility_24
-                    else R.drawable.baseline_visibility_off_24
-
-                    // Localized description for accessibility services
-                    val description = if (passwordVisible) "Hide password" else "Show password"
-
-                    // Toggle button to hide or display password
-                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
-                        Icon(painter = painterResource(id = image), description)
-                    }
-                })
-            ClickableText(
-                text = AnnotatedString("or Register"),
-                onClick = { navHostController.navigate(Screen.RegisterScreen.route){
-                    popUpTo(route = Screen.LoginScreen.route){
-                        inclusive = true
-                    }
-                } },
-                style = TextStyle(
-                    color = Color.Blue,
-                    textDecoration = TextDecoration.Underline
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.60f)
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 30.dp,
+                        topEnd = 30.dp,
+                        bottomEnd = 0.dp,
+                        bottomStart = 0.dp
+                    )
                 )
+                .background(whiteBackground)
+                .padding(10.dp)
+        ) {
+
+            Text(
+                text = "Sign In",
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    letterSpacing = 2.sp
+                ),
+                fontSize = 30.sp
             )
-            Box(modifier = Modifier.fillMaxSize()) {
-                ElevatedButton(
+            Spacer(modifier = Modifier.padding(20.dp))
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                OutlinedTextField(
+                    value = viewModel.email.value,
+                    onValueChange = { viewModel.email.value = it },
+                    label = { Text(text = "Email Address") },
+                    placeholder = { Text(text = "Email Address") },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(0.8f),
+                    keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+                    keyboardActions = KeyboardActions(onNext = {
+                        focusManager.moveFocus(
+                            FocusDirection.Down
+                        )
+                    })
+                )
+
+                OutlinedTextField(
+                    value = viewModel.password.value,
+                    onValueChange = { viewModel.password.value = it },
+                    trailingIcon = {
+                        IconButton(onClick = {
+                            passwordVisible = !passwordVisible
+                        }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.baseline_visibility_24),
+                                contentDescription = "",
+                                tint = if (passwordVisible) MaterialTheme.colorScheme.primary else Color.Gray,
+
+                                )
+                        }
+                    },
+                    label = { Text("Password") },
+                    placeholder = { Text(text = "Min 6 character") },
+                    singleLine = true,
+                    visualTransformation = if (passwordVisible) VisualTransformation.None
+                    else PasswordVisualTransformation(),
                     modifier = Modifier
-                        .align(Alignment.BottomEnd)
-                        .padding(5.dp),
+                        .fillMaxWidth(0.8f)
+                        .focusRequester(focusRequester = focusRequester),
+                )
+
+                Spacer(modifier = Modifier.padding(10.dp))
+                Button(
+                    enabled = viewModel.email.value.isNotBlank() && viewModel.password.value.length >= 6,
                     onClick = {
                         viewModel.login(
                             onSuccess = {
-                                navHostController.navigate(Screen.HomeScreen.route) {
+                                navController.navigate(Screen.HomeScreen.route) {
                                     popUpTo(
                                         Screen.LoginScreen.route,
                                     ) { inclusive = true }
@@ -95,15 +150,28 @@ fun LoginScreen(navHostController: NavHostController){
                             },
                         )
                     },
-                    colors = ButtonDefaults.buttonColors(Color.Gray)
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .height(50.dp)
                 ) {
-                    Text(
-                        text = "Continue", modifier = Modifier
-                            .padding(8.dp)
-                            .width(IntrinsicSize.Max)
-                    )
+                    Text(text = "Sign In", fontSize = 20.sp)
                 }
+
+                Spacer(modifier = Modifier.padding(20.dp))
+                Text(
+                    text = "Create An Account",
+                    modifier = Modifier.clickable(onClick = {
+                        navController.navigate(Screen.RegisterScreen.route) {
+                            popUpTo(route = Screen.LoginScreen.route) {
+                                inclusive = true
+                            }
+                        }
+                    })
+                )
+                Spacer(modifier = Modifier.padding(20.dp))
             }
+
+
         }
     }
 
--- app/src/main/java/com/example/attendme/screens/RegisterScreen.kt
+++ app/src/main/java/com/example/attendme/screens/RegisterScreen.kt
@@ -1,26 +1,34 @@
 package com.example.attendme.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.attendme.R
 import com.example.attendme.model.Department
 import com.example.attendme.navigation.Screen
+import com.example.attendme.ui.theme.whiteBackground
 import com.example.attendme.viewModels.RegisterScreenViewModel
 
 
@@ -29,157 +37,177 @@ import com.example.attendme.viewModels.RegisterScreenViewModel
 fun RegisterScreen(navHostController: NavHostController) {
     val viewModel: RegisterScreenViewModel = viewModel()
     var passwordVisible by remember { mutableStateOf(false) }
-    var passwordVisible1 by remember { mutableStateOf(false) }
+    val passwordVisible1 by remember { mutableStateOf(false) }
     var expanded by remember { mutableStateOf(false) }
     var selectedOptionText by remember { mutableStateOf("Select department") }
     val context = LocalContext.current
+    val image = painterResource(id = R.drawable.register_page)
 
-    Surface(modifier = Modifier.fillMaxSize()) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Image(image, "")
+        }
 
         Column(
             modifier = Modifier
-                .fillMaxSize(),
+                .fillMaxWidth()
+                .fillMaxHeight(0.70f)
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 30.dp,
+                        topEnd = 30.dp,
+                        bottomEnd = 0.dp,
+                        bottomStart = 0.dp
+                    )
+                )
+                .background(whiteBackground)
+                .padding(10.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            OutlinedTextField(
-                value = viewModel.name.value,
-                label = { Text(text = "Name") },
-                onValueChange = {
-                    viewModel.name.value = it
-                },
-                modifier = Modifier.padding(bottom = 10.dp)
-            )
-            OutlinedTextField(
-                value = viewModel.email.value,
-                label = { Text(text = "Email") },
-                onValueChange = {
-                    viewModel.email.value = it
-                },
-                modifier = Modifier.padding(bottom = 10.dp)
-            )
-
-            ExposedDropdownMenuBox(
-                expanded = expanded,
-                onExpandedChange = {
-                    expanded = !expanded
-                }
-            ) {
-                OutlinedTextField(
-                    readOnly = true,
-                    value = selectedOptionText,
-                    onValueChange = { },
-                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-                    colors = ExposedDropdownMenuDefaults.textFieldColors()
+            Text(
+                text = "Sign Up",
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                    letterSpacing = 2.sp
                 )
-                ExposedDropdownMenu(
+            )
+            Spacer(modifier = Modifier.padding(20.dp))
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                OutlinedTextField(
+                    value = viewModel.name.value,
+                    label = { Text(text = "Name") },
+                    onValueChange = {
+                        viewModel.name.value = it
+                    },
+                    modifier = Modifier.padding(bottom = 10.dp)
+                )
+                OutlinedTextField(
+                    value = viewModel.email.value,
+                    label = { Text(text = "Email") },
+                    onValueChange = {
+                        viewModel.email.value = it
+                    },
+                    modifier = Modifier.padding(bottom = 10.dp)
+                )
+
+                ExposedDropdownMenuBox(
                     expanded = expanded,
-                    onDismissRequest = {
-                        expanded = false
+                    onExpandedChange = {
+                        expanded = !expanded
                     }
                 ) {
-                    Department.values().forEach { selectionOption ->
-                        DropdownMenuItem(
-                            onClick = {
-                                viewModel.department.value = selectionOption
-                                selectedOptionText = selectionOption.name
-                                expanded = false
-                            },
-                            text = {
-                                Text(text = selectionOption.name)
-                            },
-                        )
-                    }
-                }
-            }
-
-            OutlinedTextField(value = viewModel.password.value,
-                label = { Text(text = "Password") },
-                placeholder = { Text(text = "Minimum 6 chars") },
-                isError = viewModel.password.value.length < 6,
-                onValueChange = {
-                    viewModel.password.value = it
-                },
-                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-                trailingIcon = {
-                    val image = if (passwordVisible)
-                        R.drawable.baseline_visibility_24
-                    else R.drawable.baseline_visibility_off_24
-
-                    // Localized description for accessibility services
-                    val description = if (passwordVisible) "Hide password" else "Show password"
-
-                    // Toggle button to hide or display password
-                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
-                        Icon(painter = painterResource(id = image), description)
-                    }
-                })
-            OutlinedTextField(value = viewModel.rePassword.value,
-                label = { Text(text = "Re-Password") },
-                onValueChange = {
-                    viewModel.rePassword.value = it
-                },
-                isError = viewModel.password.value != viewModel.rePassword.value,
-                visualTransformation = if (passwordVisible1) VisualTransformation.None else PasswordVisualTransformation(),
-                trailingIcon = {
-                    val image = if (passwordVisible1)
-                        R.drawable.baseline_visibility_24
-                    else R.drawable.baseline_visibility_off_24
-
-                    // Localized description for accessibility services
-                    val description = if (passwordVisible1) "Hide password" else "Show password"
-
-                    // Toggle button to hide or display password
-                    IconButton(onClick = { passwordVisible1 = !passwordVisible1 }) {
-                        Icon(painter = painterResource(id = image), description)
-                    }
-                })
-
-            ClickableText(
-                text = AnnotatedString("Login"),
-                onClick = {
-                    navHostController.navigate(Screen.LoginScreen.route) {
-                        popUpTo(Screen.RegisterScreen.route) {
-                            inclusive = true
+                    OutlinedTextField(
+                        readOnly = true,
+                        value = selectedOptionText,
+                        onValueChange = { },
+                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                        colors = ExposedDropdownMenuDefaults.textFieldColors()
+                    )
+                    ExposedDropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = {
+                            expanded = false
+                        }
+                    ) {
+                        Department.values().forEach { selectionOption ->
+                            DropdownMenuItem(
+                                onClick = {
+                                    viewModel.department.value = selectionOption
+                                    selectedOptionText = selectionOption.name
+                                    expanded = false
+                                },
+                                text = {
+                                    Text(text = selectionOption.name)
+                                },
+                            )
                         }
                     }
-                },
-                style = TextStyle(
-                    color = Color.Blue,
-                    textDecoration = TextDecoration.Underline
-                )
-            )
+                }
 
-            ElevatedButton(
-                modifier = Modifier
-                    .padding(5.dp)
-                    .weight(1f, false),
-                onClick = {
-                    viewModel.register(
-                        onSuccess = {
-                            navHostController.navigate(Screen.HomeScreen.route) {
-                                popUpTo(
-                                    Screen.LoginScreen.route,
-                                ) { inclusive = true }
-                            }
-                        },
-                    ) {
-                        Toast.makeText(
-                            context,
-                            "Some error: $it",
-                            Toast.LENGTH_LONG,
-                        ).show()
-                    }
-                },
-                colors = ButtonDefaults.buttonColors(Color.Gray)
-            ) {
+                OutlinedTextField(value = viewModel.password.value,
+                    label = { Text(text = "Password") },
+                    placeholder = { Text(text = "Minimum 6 chars") },
+                    isError = viewModel.password.value.length < 6,
+                    onValueChange = {
+                        viewModel.password.value = it
+                    },
+                    visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                    trailingIcon = {
+                        IconButton(onClick = {
+                            passwordVisible = !passwordVisible
+                        }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.baseline_visibility_24),
+                                contentDescription = "",
+                                tint = if (passwordVisible) MaterialTheme.colorScheme.primary else Color.Gray,
+                            )
+                        }
+                    })
+                OutlinedTextField(value = viewModel.rePassword.value,
+                    label = { Text(text = "Re-Password") },
+                    onValueChange = {
+                        viewModel.rePassword.value = it
+                    },
+                    isError = viewModel.password.value != viewModel.rePassword.value,
+                    visualTransformation = if (passwordVisible1) VisualTransformation.None else PasswordVisualTransformation(),
+                    trailingIcon = {
+                        IconButton(onClick = {
+                            passwordVisible = !passwordVisible
+                        }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.baseline_visibility_24),
+                                contentDescription = "",
+                                tint = if (passwordVisible) MaterialTheme.colorScheme.primary else Color.Gray,
+                            )
+                        }
+                    })
+                ElevatedButton(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .weight(1f, false),
+                    onClick = {
+                        viewModel.register(
+                            onSuccess = {
+                                navHostController.navigate(Screen.HomeScreen.route) {
+                                    popUpTo(
+                                        Screen.LoginScreen.route,
+                                    ) { inclusive = true }
+                                }
+                            },
+                        ) {
+                            Toast.makeText(
+                                context,
+                                "Some error: $it",
+                                Toast.LENGTH_LONG,
+                            ).show()
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(Color.Gray)
+                ) {
+                    Text(
+                        text = "Sign Up", modifier = Modifier
+                            .padding(8.dp)
+                            .width(IntrinsicSize.Max)
+                    )
+                }
+                Spacer(modifier = Modifier.width(8.dp))
                 Text(
-                    text = "Continue", modifier = Modifier
-                        .padding(8.dp)
-                        .width(IntrinsicSize.Max)
+                    text = "Login Instead",
+                    modifier = Modifier.clickable(onClick = {
+                        navHostController.navigate(Screen.LoginScreen.route) {
+                            popUpTo = navHostController.graph.startDestinationId
+                            launchSingleTop = true
+                        }
+                    })
                 )
             }
-
 
         }
     }
